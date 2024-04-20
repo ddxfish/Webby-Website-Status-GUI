9d40d5ef234903cdd0ac35e2eec2dd7f088e2450
ddxfish <ddxfish@gmail.com>
2024-04-20 01:51:15 -0400

    removed useless files

--- settings.py
+++
@@ -1,6 +0,0 @@
-# settings.py
-checkInterval = 60000 * 70  # how often we check sites, only change the second number
-guiInterval = 60000 # how often we update the gui in milliseconds
-
-startWidth = 720
-startHeight = 1280
